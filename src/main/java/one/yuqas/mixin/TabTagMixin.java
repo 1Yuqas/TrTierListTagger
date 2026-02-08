5c370c1e3fb13e709ae7268abae51017fd4b3dfe
ekky25 <151433965+ekky25@users.noreply.github.com>
2026-02-08 16:38:15 +0300

    .

--- src/main/java/one/yuqas/mixin/TabTagMixin.java
+++ src/main/java/one/yuqas/mixin/TabTagMixin.java
@@ -25,9 +25,9 @@ public class TabTagMixin {
         Text baseName = original;
         if (baseName == null) {
             if (self.getScoreboardTeam() != null) {
-                baseName = self.getScoreboardTeam().decorateName(Text.literal(self.getProfile().name()));
+                baseName = self.getScoreboardTeam().decorateName(Text.literal(self.getProfile().getName()));
             } else {
-                baseName = Text.literal(self.getProfile().name());
+                baseName = Text.literal(self.getProfile().getName());
             }
         }
 
@@ -65,7 +65,7 @@ public class TabTagMixin {
         try {
             PlayerListEntry self = (PlayerListEntry) (Object) this;
             if (self.getProfile() == null) return null;
-            return self.getProfile().name();
+            return self.getProfile().getName();
         } catch (Exception e) {
             return null;
         }
