b832a5a471f6339c89a6531375ec5c37851ef754
ekky25 <151433965+ekky25@users.noreply.github.com>
2026-02-08 15:57:03 +0300

    Update TierConfigScreen.java

--- src/main/java/one/yuqas/utils/ui/TierConfigScreen.java
+++ src/main/java/one/yuqas/utils/ui/TierConfigScreen.java
@@ -3,7 +3,6 @@ package one.yuqas.utils.ui;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.input.KeyInput;
 import net.minecraft.text.Text;
 import one.yuqas.utils.TierConfig;
 import one.yuqas.utils.enums.Config;
@@ -130,11 +129,11 @@ public class TierConfigScreen extends Screen {
     }
 
     @Override
-    public boolean keyPressed(KeyInput input) {
-        if (input.key() == GLFW.GLFW_KEY_ESCAPE) {
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (keyCode == GLFW.GLFW_KEY_ESCAPE) {
             this.client.setScreen(parent);
             return true;
         }
-        return super.keyPressed(input);
+        return super.keyPressed(keyCode, scanCode, modifiers);
     }
 }
