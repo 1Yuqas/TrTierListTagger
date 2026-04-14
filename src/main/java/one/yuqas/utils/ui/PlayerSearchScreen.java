92e1a0e0dd5526783cffb929f5d9a1b845e10c92
ekky25 <151433965+ekky25@users.noreply.github.com>
2026-04-14 10:22:56 +0300

    Update PlayerSearchScreen.java

--- src/main/java/one/yuqas/utils/ui/PlayerSearchScreen.java
+++ src/main/java/one/yuqas/utils/ui/PlayerSearchScreen.java
@@ -6,7 +6,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.PlayerSkinWidget;
 import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.texture.SkinTextures;
+import net.minecraft.client.util.SkinTextures;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import java.util.List;
