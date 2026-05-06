3f12f6419f584e6add586edd2600f57930668fd8
Yuqas <152551251+1Yuqas@users.noreply.github.com>
2026-05-07 00:23:57 +0300

    VersionChecker eklendi ve güncellendi, Bilinmiyor -> Bilinmeyen düzeltildi

--- src/main/java/one/yuqas/TrTierListTagger.java
+++ src/main/java/one/yuqas/TrTierListTagger.java
@@ -4,12 +4,15 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import one.yuqas.command.TierCommand;
 import one.yuqas.utils.TierConfigUtil;
+import one.yuqas.utils.VersionChecker;
 
 public class TrTierListTagger implements ModInitializer {
 
     @Override
     public void onInitialize() {
         TierConfigUtil.load();
+
+        VersionChecker.check();
 
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             TierCommand.register(dispatcher);
