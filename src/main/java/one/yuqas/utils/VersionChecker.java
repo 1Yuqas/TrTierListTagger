9be505af776c0661668e64bb5d7f017bf26aa2d7
Yuqas <152551251+1Yuqas@users.noreply.github.com>
2026-05-07 00:50:46 +0300

    added update checker

---
+++ src/main/java/one/yuqas/utils/VersionChecker.java
@@ -0,0 +1,72 @@
+package one.yuqas.utils;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.fabricmc.loader.api.FabricLoader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class VersionChecker {
+    public static final String PROJECT_ID = "tgKll25i";
+    public static String latestVersion = "Bilinmeyen";
+    public static String updateUrl = "";
+    public static boolean updateAvailable = false;
+
+    public static void check() {
+        try {
+            String mcVersion = FabricLoader.getInstance().getModContainer("minecraft").get().getMetadata().getVersion().getFriendlyString();
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://api.modrinth.com/v2/project/" + PROJECT_ID + "/version"))
+                    .header("User-Agent", "TrTierListTagger/Updater")
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                JsonArray versions = JsonParser.parseString(response.body()).getAsJsonArray();
+
+                for (JsonElement element : versions) {
+                    JsonObject versionObj = element.getAsJsonObject();
+                    JsonArray gameVersions = versionObj.getAsJsonArray("game_versions");
+
+                    boolean isCompatible = false;
+                    for (JsonElement gv : gameVersions) {
+                        if (gv.getAsString().equals(mcVersion)) {
+                            isCompatible = true;
+                            break;
+                        }
+                    }
+
+                    if (isCompatible) {
+                        latestVersion = versionObj.get("version_number").getAsString();
+
+                        JsonArray files = versionObj.getAsJsonArray("files");
+                        for (JsonElement fileElement : files) {
+                            JsonObject fileObj = fileElement.getAsJsonObject();
+                            if (fileObj.get("primary").getAsBoolean()) {
+                                updateUrl = fileObj.get("url").getAsString();
+                                break;
+                            }
+                        }
+
+                        String currentModVersion = FabricLoader.getInstance()
+                                .getModContainer("trtierlisttagger").get().getMetadata().getVersion().getFriendlyString();
+
+                        if (!currentModVersion.equals(latestVersion)) {
+                            updateAvailable = true;
+                        }
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
