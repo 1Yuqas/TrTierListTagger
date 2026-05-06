23b994ef006633204635f8b515aaaa861567ef50
Yuqas <152551251+1Yuqas@users.noreply.github.com>
2026-05-07 01:04:25 +0300

    Update TrTierListTagger.java

--- src/main/java/one/yuqas/TrTierListTagger.java
+++ src/main/java/one/yuqas/TrTierListTagger.java
@@ -2,17 +2,28 @@ package one.yuqas;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.gui.screen.TitleScreen;
 import one.yuqas.command.TierCommand;
 import one.yuqas.utils.TierConfigUtil;
 import one.yuqas.utils.VersionChecker;
+import one.yuqas.utils.ui.UpdateScreen;
 
 public class TrTierListTagger implements ModInitializer {
+    private boolean hasShownUpdate = false;
 
     @Override
     public void onInitialize() {
         TierConfigUtil.load();
 
         new Thread(VersionChecker::check).start();
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (VersionChecker.updateAvailable && !hasShownUpdate && client.currentScreen instanceof TitleScreen) {
+                client.setScreen(new UpdateScreen(client.currentScreen));
+                hasShownUpdate = true;
+            }
+        });
 
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             TierCommand.register(dispatcher);
